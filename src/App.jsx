983802b5bfef03b30002243adcff46c20bf93bd1
khan-097 <146518559+khan-097@users.noreply.github.com>
2026-05-16 22:39:47 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Testimonials from "./sections/Testimonials";
 import Footer from "./sections/Footer";
 import Contact from "./sections/Contact";
 import TechStack from "./sections/TechStack";
@@ -16,7 +15,6 @@ const App = () => (
     <FeatureCards />
     <Experience />
     <TechStack />
-    <Testimonials />
     <Contact />
     <Footer />
   </>
