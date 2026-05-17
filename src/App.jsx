75c3c7eb3990516ab3e62ea2fc7e8facbb8e5995
khan-097 <146518559+khan-097@users.noreply.github.com>
2026-05-17 06:53:09 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Contact from "./sections/Contact";
 import TechStack from "./sections/TechStack";
 import Experience from "./sections/Experience";
 import Hero from "./sections/Hero";
-import LogoShowcase from "./sections/LogoShowcase";
+import ShowcaseSection from "./sections/ShowcaseSection";
 import FeatureCards from "./sections/FeatureCards";
 import Navbar from "./components/NavBar";
 
